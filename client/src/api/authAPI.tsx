1afc3dcfaaa62bba3f46a978c4c347a2e97b3bd0
Meez <meez@Meezs-MBP.fios-router.home>
2024-12-04 17:31:40 -0600

    fixed login api in client

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,21 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  const response = await fetch(
+    '/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userInfo)
+  }
+  )
+  const data = response.json();
+
+  if (!response.ok) {
+    throw new Error('invalid API response, check network tab!');
+  }
+  return data;
 }
-
-
 
 export { login };
